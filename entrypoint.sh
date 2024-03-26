ccfe5658cde9169026c31147927d4f63a890add5
suneel-lean <suneel.subramanya@leantech.me>
2024-03-27 02:30:45 +0530

    QA-1278: updated entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# Run your conformance.js script
+# Run your service in the background
 nohup npm run service &
+PID1=$!
 
-# Run the wiretap command
+# Run the wiretap command in the background
 nohup npm run wiretap &
+PID2=$!
+
+# Wait for any of the background processes to finish, keeping the container alive
+wait $PID1 $PID2
